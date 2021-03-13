73e478e301772a4c7430a0f6d38dfe69b8fdafeb
Zak <zakmikellides@gmail.com>
2021-03-13 16:36:27 +0000

    Switch to Webpack and Babel for running on IE11 (CSS is broken currently)

---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const babelLoaderExcludeNodeModulesExcept = require('babel-loader-exclude-node-modules-except');
+
+module.exports = {
+    entry: ["./src/index.tsx"],
+    output: {
+        filename: "pka-index-frontend.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx|js|jsx)$/i,
+                exclude: babelLoaderExcludeNodeModulesExcept([
+                    'scroll-js'
+                ]),
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            ["@babel/preset-env", {
+                                useBuiltIns: 'entry',
+                                corejs: 3,
+                            }],
+                            "@babel/react",
+                            "@babel/preset-typescript",
+                        ],
+                        plugins: ["transform-class-properties"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".tsx", ".ts", ".json"]
+    }
+}
